958ff86a17fcb6ce75b61eb88fccb5a42503029d
Dave Carroll <djcarroll@gmail.com>
2016-05-18 14:27:28 +0100

    test

--- AdyenRecurringExamples/src/Credentials.java
+++ AdyenRecurringExamples/src/Credentials.java
@@ -1,6 +1,6 @@
 
 public class Credentials {
-
+	//THis is test
 	static protected String test_address = "https://pal-test.adyen.com/pal/servlet/soap/Payment";
     static protected String live_address = "https://pal-live.adyen.com/pal/servlet/soap/Payment";
 
